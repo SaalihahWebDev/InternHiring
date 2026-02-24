7815156d7bdc0e6db7f20a798c0a1fbc4bbc4fe6
SaalihahWebDev <bashirabanu91@gmail.com>
2026-02-24 04:37:27 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class main {
+    public static Candidate getCandidateDetails() throws InvalidInternException {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the Candidate details:");
+        System.out.println("Name:");
+        String name=scanner.next();
+        System.out.println("Gender:");
+        String gender=scanner.next();
+        System.out.println("Enter percentage in 10th:");
+        int percentage=scanner.nextInt();
+        if (percentage<50){
+            throw new InvalidInternException("Registration failed");
+        }else{
+            Candidate canditate=new Candidate();
+            canditate.setName(name);
+            canditate.setGender(gender);
+            canditate.setPercentage(percentage);
+            return canditate;
+        }
+    }
+}
